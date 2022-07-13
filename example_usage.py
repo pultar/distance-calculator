77dc1b77f96fc6052b8adf6c91364a502c6f2deb
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2022-07-13 15:24:24 +0200

    Added another example
    Signed-off-by: Felix Pultar <felix.pultar@phys.chem.ethz.ch>

--- example_usage.py
+++ example_usage.py
@@ -1,4 +1,6 @@
-from distance import distance_numpy as calc_dist
+from pygromos.files.coord.cnf import Cnf
+from distance import distance_numpy as calc_dist_1
+from distance import distance_pygromos as calc_dist_2
 
 import numpy as np
 
@@ -8,5 +10,9 @@ array_2 = np.random.rand(3)
 print(array_1)
 print(array_2)
 
-distance = calc_dist(array_1, array_2)
-print(f"Distance calculated: {distance}")
+distance_1 = calc_dist_1(array_1, array_2)
+print(f"Distance calculated from random arrays: {distance_1}")
+
+my_configuration = Cnf("examples/menthol.cnf")
+distance_2 = calc_dist_2(my_configuration, 1, 30)
+print(f"Distance calculated between atoms 1 and 30 for menthol.cnf: {distance_2} nm")
